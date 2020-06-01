5a22d00334b77d2e63a4a7841568ed829b294bc5
mayuki-k <ymtech0615@gmail.com>
2020-06-01 23:52:26 +0900

    Pathパラメータのバリデータ追加

--- path_parameter/app.py
+++ path_parameter/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import uvicorn
 
 app = FastAPI()
@@ -10,6 +10,14 @@ def index():
 @app.get("/path/{id}")
 def path(id:int):
     return {"value":id}
+
+@app.get("/items/{id}")
+def items(id:int = Path(..., gt=5, le=10)):
+    return {"id":id}
+
+@app.get("/items2/{id}")
+def items2(id:int = Path(..., ge=5, lt=10)):
+    return {"id":id}
 
 if __name__ == "__main__":
     uvicorn.run("app:app")
